0bed0be0baf584daa2328eaf8c3188245666de92
Forz70043 <alfonso.pisicchio@libero.it>
2020-06-11 16:29:59 +0200

    clean script

--- script.js
+++ script.js
@@ -9,28 +9,21 @@ $(document).ready(function() {
     var stories=getStoriesLanguage();
     writeStories(stories,storiesIndex);
 
+
 });
 
 	function writeStories(stories,storiesIndex)
     {
-        console.log("writeStories");
-        console.log("stories index");
-        console.log(storiesIndex);
         var word = ' '+stories[storiesIndex];
-        console.log(word);
 	    var index=0;
     	
         var adder = setInterval(function(){ 
             typeWriter.innerHTML = typeWriter.innerHTML + word[index];
             index++;
     		if(index===word.length) {
-        		//console.log("index ===word.length");
         		clearInterval(adder);
         		if(storiesIndex !== stories.length-1) {
-            		//console.log("indice storie != storia.length-1"); 
-            		//setInterval(function(){
-        				//typeWriter.innerHTML = ''; 
-        			//},1000);
+            		//setInterval(function(){ typeWriter.innerHTML = ''; },1000);
             		deleteStories(stories,storiesIndex);
             		//viewProject();
         		}
@@ -40,20 +33,13 @@ $(document).ready(function() {
 
 	function deleteStories(stories,storiesIndex,del=false)
     {
-        console.log("delete");
     	var backSpacer = setInterval(function(){
         	//se vuoi eliminare decommenta
             //typeWriter.innerHTML = typeWriter.innerHTML.substring(0, typeWriter.innerHTML.length-1);
-        	console.log("typeWriter length");
-            console.log(typeWriter.innerHTML.length);
-            //
             storiesIndex++;
             if(del){
-                if(!typeWriter.innerHTML.length) 
-                {
-                    console.log("typeWriter length false");
+                if(!typeWriter.innerHTML.length) {
                     storiesIndex++;
-                    console.log(storiesIndex);
                     clearInterval(backSpacer);
                     writeStories(stories,storiesIndex);
                 }
@@ -73,9 +59,7 @@ $(document).ready(function() {
     function getStoriesLanguage(){
         var userLang = navigator.language || navigator.userLanguage;
         var color = ["white"];
-        console.log(userLang);
-        console.log(userLang.slice(0,2));
-        console.log("getStries");
+
         switch(userLang.slice(0,2)){
             case "en-US":
             case "en-GB":
@@ -91,6 +75,5 @@ $(document).ready(function() {
                 var stories = ["Добро пожаловать, я Альфонсо или, вернее, я веб-сервер Альфонсо. Здесь вы найдете его портфолио небольших личных проектов и его биографические данные. Спасибо за то, что вы здесь. Dobro pozhalovat', ya Al'fonso ili, verneye, ya veb-server Al'fonso. Zdes' vy naydete yego portfolio nebol'shikh lichnykh proyektov i yego biograficheskiye dannyye. Spasibo za to, chto vy zdes'. &hearts;"];
                 break;
         }
-        console.log(stories);
         return stories;
     }
