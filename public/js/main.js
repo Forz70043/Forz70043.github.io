06ce0fe42bef0a5bdd572e53656e14793af301e7
Forz70043 <alfonso.pisicchio@libero.it>
2025-02-02 21:09:01 +0100

    Added stack section

--- public/js/main.js
+++ public/js/main.js
@@ -8,23 +8,15 @@ function getStoriesLanguage(){
 }
 
 window.onload = (event) => {
-    //console.log('page is fully loaded');
-    
-    var stories = ['Developer', 'OpenSource Lover', 'Programmer', 'Backend Developer', 'Frontend Developer'];
-    //console.log("stories", stories);
-
     var app = document.getElementById('whoIam');
  
     var typewriter = new Typewriter(app, {
         loop: true,
         delay: 75,
     });
-    let age = Math.floor((new Date() - new Date('1993-07-17').getTime()) / 3.15576e+10 ); 
+    
     typewriter
     .pauseFor(1500)
-    //.typeString('<strong>Welcome to my personal digital garden,</br>A compendium of what I\'ve created and learned over the years</strong>')
-    //.pauseFor(1500)
-    //.deleteAll()
     .typeString('<strong>Developer</strong>')
     .pauseFor(500)
     .deleteAll()
@@ -34,9 +26,6 @@ window.onload = (event) => {
     .typeString('<strong>Programmer</strong>')
     .pauseFor(500)
     .deleteChars(10)
-    /*.typeString('<strong>var Alfonso = { </br>  surname: Pisicchio,</br>  age: '+ age +
-    ',</br>skills: PHP, Js, Bash, MySql/MariaDB, NodeJs, Express, API</br></strong>')
-    */
    .typeString('Sunset & SUP lover')
    .pauseFor(500)
    .deleteAll()
